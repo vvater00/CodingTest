186f1d6bf6f72e9f496b10d7f72ef24199fe59d5
guddn <hwch0901@naver.com>
2026-01-27 21:24:34 +0900

    백준 1934

---
+++ week01/src/week01/Boj_1934.java
@@ -0,0 +1,37 @@
+package week01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_1934 {//최소공배수 구하기 and 최대공약수
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb =new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int x = UCL(a, b);
+			sb.append((a * b) / x).append("\n");
+			
+		}
+		bw.write(sb+"");
+		bw.close();
+
+	}
+	
+	public static int UCL(int a, int b) {
+		if (b == 0)
+			return a;
+		return UCL(b, a % b);
+	}
+
+
+}
