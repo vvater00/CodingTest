d98b5c376843999f104cdec3eba543980639a5bb
guddn <hwch0901@naver.com>
2026-01-27 21:32:26 +0900

    백준 1934 문제 수정

--- week01/src/week01/Boj_1934.java
+++ week01/src/week01/Boj_1934.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Boj_1934 {//최소공배수 구하기 and 최대공약수
+public class Boj_1934 {//최소 공배수 구하기 and 최대 공약수
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
